f452bff7748c1d06f15bf4fe0d52504e09142a78
smutallib97 <smutallib97@gmail.com>
2022-12-09 00:53:19 +0530

    Added: Check Employee is Present or Absent

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main(){
 
     cout<< "\nWelcome to Employee Wage Computation\n"<<endl;
+
+    int IS_PRESENT = 1;
+    int empCheck;
+    srand(time(0));
+    empCheck = rand() % 2;
+    if (empCheck == 1)
+        cout << "Employee is Present" << endl;
+    else
+        cout << "Employee is Absent" << endl;
 }
