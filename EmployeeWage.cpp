10e4ae681b62cabe02e2b20f8b963b4bfd1d5f5c
smutallib97 <smutallib97@gmail.com>
2022-12-09 01:03:56 +0530

    Added: Calculate Daily Employee Wage

--- EmployeeWage.cpp
+++ EmployeeWage.cpp
@@ -4,15 +4,27 @@
 using namespace std;
 
 int main(){
+    
+    const int RATE_PER_HOUR = 20;
+    int empHrs;
+    int empWage;
+    int IS_PRESENT = 1;
+    int empCheck;
 
     cout<< "\nWelcome to Employee Wage Computation\n"<<endl;
 
-    int IS_PRESENT = 1;
-    int empCheck;
     srand(time(0));
     empCheck = rand() % 2;
-    if (empCheck == 1)
+    if (empCheck == 1){
         cout << "Employee is Present" << endl;
-    else
+        empHrs = 8;
+    }
+    else{
         cout << "Employee is Absent" << endl;
+        empHrs = 0;
+    }
+    empWage = empHrs * RATE_PER_HOUR;
+    cout << "Employee Wage Per Day = " << empWage << endl;
+
+     return 0;   
 }
